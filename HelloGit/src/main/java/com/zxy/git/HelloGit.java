b1d66e29d3f5fe7dbc9fa1183866bddf2caeefd6
yun-cell <3174966139@qq.com>
2021-04-07 10:45:12 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/zxy/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.zxy.git;
+
+/**
+ * @author Thinkpad
+ * @date 2021/4/7 - 10:34
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!");
+    }
+}
